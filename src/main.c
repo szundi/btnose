18dd9780189bc5c17c424fd3c6e002c5b9b3340c
foldedtoad <foldedtoad@gmail.com>
2019-12-09 19:11:56 -0800

    build changes.

--- src/main.c
+++ src/main.c
@@ -70,28 +70,28 @@ static void process_sample(struct device *dev)
 	static unsigned int obs;
 	struct sensor_value temp, hum;
 	if (sensor_sample_fetch(dev) < 0) {
-		printf("Sensor sample update error\n");
+		printk("Sensor sample update error\n");
 		return;
 	}
 
 	if (sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp) < 0) {
-		printf("Cannot read HTS221 temperature channel\n");
+		printk("Cannot read HTS221 temperature channel\n");
 		return;
 	}
 
 	if (sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &hum) < 0) {
-		printf("Cannot read HTS221 humidity channel\n");
+		printk("Cannot read HTS221 humidity channel\n");
 		return;
 	}
 
 	++obs;
-	printf("Observation:%u\n", obs);
+	printk("Observation:%u\n", obs);
 
 	/* display temperature */
-	printf("Temperature:%.1f C\n", sensor_value_to_double(&temp));
+	printk("Temperature:%.1f C\n", sensor_value_to_double(&temp));
 
 	/* display humidity */
-	printf("Relative Humidity:%.1f%%\n",
+	printk("Relative Humidity:%.1f%%\n",
 	       sensor_value_to_double(&hum));
 }
 
@@ -104,7 +104,7 @@ void main(void)
 
 	struct device *dev = device_get_binding("HTS221");
 	if (dev == NULL) {
-		printf("Could not get HTS221 device\n");
+		printk("Could not get HTS221 device\n");
 		return;
 	}
 
